e8b2c1d3de75c7992856d42a251885151cdab1ba
umaz <maz.tsukuba@gmail.com>
2016-06-13 16:49:53 +0900

    認証

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612122442) do
+ActiveRecord::Schema.define(version: 20160613073812) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +40,13 @@ ActiveRecord::Schema.define(version: 20160612122442) do
     t.string   "group"
     t.text     "about"
     t.string   "kind"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
